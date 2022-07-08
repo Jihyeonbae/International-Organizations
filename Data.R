ca3188c9bdf414b8594ffd2d8150b926d2d2946a
Jihyeon Bae <jihyeonb@uw.edu>
2022-07-07 20:05:21 -0700

    commit

--- Data.R
+++ Data.R
@@ -3,7 +3,7 @@
 library("pacman")
 p_load(plyr, dplyr, ggplot2, MASS, tidyverse, magrittr, RColorBrewer, readxl,
        readr, rtweet, lubridate, scales, leaflet, academictwitteR, ggmap, quanteda,
-       ggmap, jtools, tidytest, googledrive, palmerpenguins)
+       ggmap, jtools, tidytest, googledrive, palmerpenguins, haven)
 
 qog<-read_csv("/Volumes/GoogleDrive/My Drive/IGO/Data/QOG/qog_bas_ts_jan19.csv")
 vdem<-read_csv("/Volumes/GoogleDrive/My Drive/IGO/Data/VDEM/V-Dem-CY-Full+Others-v9.csv")
@@ -14,12 +14,12 @@ ccode <- read_dta("/Volumes/GoogleDrive/My Drive/IGO/Data/COW/IGO_stata/state_ye
 
 # country data
 qog <-qog %>%
-  dplyr::select(cname, year, ccodecow, wdi_gdpcapcon2010, cspf_sfi, iiag_rol, wdi_pop, atop_number, ht_colonial, p_polity2, chga_demo, wdi_trade, ht_regtype1, vdem_libdem, vdem_polyarchy)%>%
+  dplyr::select(cname, year, ccodecow, wdi_gdpcapcon2010, cspf_sfi, iiag_rol, wdi_pop, atop_number, p_polity2, chga_demo, wdi_trade, vdem_polyarchy)%>%
   dplyr::rename(cow_ccode=ccodecow)%>%
   dplyr::relocate(cow_ccode, cname, year)
 
 #outer merge of IGO
-#first, i need to attach cow country codne to Pevehouse data
+#first, i need to attach cow country code to Pevehouse data
 ccode <- ccode %>%
   dplyr::select(ccode, state, year)%>%
   dplyr::rename(country = state)%>%
@@ -34,10 +34,10 @@ COW <- COW %>%
   pivot_longer(c(`afghanistan`:`zimbabwe`),
                       names_to="country",
                       values_to="membership")
-  
+
 COW <- COW %>%
   dplyr::filter(membership==1)%>%
-  dplyr::left_join(ccode, by=c("country","year")) 
+  dplyr::left_join(ccode, by=c("country","year"))
 
 igo <- COW %>%
   dplyr::left_join(qog, by=c("cow_ccode", "year"))
@@ -45,21 +45,19 @@ igo <- COW %>%
 
 #summarising state level info to igo level
 igo <- igo %>%
-  dplyr::group_by(cow_igocode, ioname, year) %>%
+  dplyr::group_by(cow_igocode, year) %>%
   dplyr::summarise(polyarchy=mean(vdem_polyarchy, na.rm=T),
                 libdem=mean(vdem_libdem, na.rm=T),
+                polity=mean(p_polity2, na.rm=T),
                 gdp_cap=mean(wdi_gdpcapcon2010, na.rm=T),
-                state_fragility=mean(cspf_sfi, na.rm=T),
-                rule_of_law=mean(iiag_rol, na.rm=T),
-                population=mean(wdi_pop, na.rm=T),
                 alliances=mean(atop_number, na.rm=T),
-                disparity=sd(gdp_cap, na.rm=T),
+                disparity=sd(wdi_gdpcapcon2010, na.rm=T),
                 number=n(),
                 trade=mean(wdi_trade, na.rm=T),
                 percentage=sum(chga_demo, na.rm=T)/number,
                 political=mean(political),
                 social=mean(social),
-                economic=mean(economic)) 
+                economic=mean(economic))
 
 # merging with MIA data
 
@@ -68,8 +66,9 @@ MIA <- MIA %>%
   dplyr::rename(cow_igocode = ionumber)
 
 df <- MIA %>%
-  dplyr::right_join(igo, by=c("cow_igocode", "year"))
+  dplyr::inner_join(igo, by=c("cow_igocode", "year"))
 
-ggplot(df, aes(x=year, y=libdem))+geom_line()
 
+
+saveRDS(df, file = "IGOdata.RDS")
 
---
+++ Modeling.R
@@ -0,0 +1,65 @@
+####Modeling
+
+library(plm)
+
+#wrangling
+
+igo <-readRDS("IGOdata.rds")
+igo <- igo%>%
+  dplyr::filter(year>=1950) %>%
+  dplyr::mutate(AIGO = ifelse(polyarchy < 0.5, 1, 0),
+                DIGO = ifelse(polyarchy >-0.5, 1, 0),
+                AIGO_polity = ifelse(polity < 6, 1, 0 )) %>%
+  dplyr::mutate_all(~ifelse(is.nan(.), NA, .)) %>%
+  dplyr::na.omit(polyarchy, poolconstit) %>%
+  dplyr::arrange(ioname, year) %>%
+  dplyr::select(ioname, year, polyarchy, poolconstit, delconstit, everything())%>%
+  as.data.frame()
+
+#Model1) Within-FE Model
+model1 <-plm(poolconstit
+                 ~ AIGO + trade + log(gdp_cap) + number + alliances + disparity,
+            data = igo,
+            index = c("cow_igocode", "year"),
+            model = "within",
+            effect = "individual"
+)
+summary(model1, vcovBK(model1))
+
+
+#Model2) Two way FE Model
+
+model2 <-plm(poolconstit
+             ~ AIGO + trade + disparity + log(gdp_cap) + number + alliances,
+             data = igo,
+             index = c("cow_igocode", "year"),
+             model = "within",
+             effect = "twoways"
+)
+
+summary(model2, vcovBK(model2))
+
+#Model3) First Difference Model
+
+model3 <-plm(poolconstit
+             ~ polyarchy + trade + disparity + log(gdp_cap) + number + social + economic
+             + alliances -1,
+             data = igo,
+             index = c("ioname", "year"),
+             model = "fd"
+)
+
+summary(model3)
+
+#Model4) Random Effects Model
+
+model4 <-plm(poolconstit
+             ~ AIGO + trade + disparity + log(gdp_cap) + number + social + economic
+             + alliances ,
+             data = igo,
+             index = c("ioname", "year"),
+             model = "random"
+)
+
+summary(model4)
+
